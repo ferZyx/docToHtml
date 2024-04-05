a249b10c5060d63014230fb68b4ad81257b7ace0
ferZyx <sorokin.wlad@yandex.ru>
2024-04-05 17:56:45 +0500

    test

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var pandoc_ts_1 = require("pandoc-ts");
-var outs = [
-    { name: "html", format: "html", fname: "sample.html" }
-];
+var outs = [{ name: "html", format: "html", fname: "sample.html" }];
 var pandocInstance = new pandoc_ts_1.Pandoc("docx", outs);
 pandocInstance.convertFile("original.docx", function (result, err) {
     if (err) {
--- app.ts
+++ app.ts
@@ -1,18 +1,40 @@
-import { Pandoc, PandocOutFormat } from "pandoc-ts";
+import {ChildProcessWithoutNullStreams, spawn} from "child_process";
 
-const outs: PandocOutFormat = { name: "html", format:"html", fname: "sample.html" };
+async function convertWordToHtmlAsync(wordName: string,args: string[] = []): Promise<void> {
+    return new Promise<void>((resolve, reject): void => {
+        // libreoffice --headless --convert-to html:HTML:EmbedImages 10.doc --outdir html
+        const commandPrompt: string[] = ['headless', '--convert-to', 'html:HTML:EmbedImages',wordName, '--outdir html', ...args];
+        console.log('commandPrompt:', commandPrompt);
+        let libreoffice: ChildProcessWithoutNullStreams = spawn("libreoffice", commandPrompt);
 
-const pandocInstance = new Pandoc("docx", outs);
+        libreoffice.on("error", (err) => {
+            console.error('Error:', err);
+            reject(new Error('Error converting file'));
+        });
 
-// pandocInstance.convertFile("input.docx", (result, err) => {
-//     if (err) {
-//         console.error(err);
-//     }
-//     if (result) {
-//         console.log(result.html);
-//     }
-// });
+        libreoffice.on("exit", (code, signal) => {
+            if (code !== 0) {
+                console.error('Ошибка конвертации:', code, signal);
+                reject(new Error('Ошибка конвертации файла'));
+            } else {
+                console.log('Конвертация завершена успешно');
+                resolve();
+            }
+        });
+    });
+}
+convertWordToHtmlAsync('original.docx')
+    .then(() => {
+        console.log('Conversion completed successfully');
+    })
+    .catch((error) => {
+        console.error('Conversion failed:', error);
+    });
 
-(async () => {
-    await pandocInstance.convertFileAsync("input.docx");
-})
+convertWordToHtmlAsync('original.doc')
+    .then(() => {
+        console.log('Conversion completed successfully');
+    })
+    .catch((error) => {
+        console.error('Conversion failed:', error);
+    });
