dd12402dac1cd9a9a833ece3f164602a2c442828
ferZyx <sorokin.wlad@yandex.ru>
2024-04-09 20:37:26 +0500

    test

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,11 @@
 import {wordToHtmlByLibreOffice} from "../wordToHtmlConverter.js";
 
-(async () => {
-    const startTime = new Date().getTime();
+const startTime = new Date().getTime();
 
+await (async () => {
     const runtime = process.argv[2]
-    if (runtime === "libreOne"){
+    console.log(runtime)
+    if (runtime === "libreOne") {
         const wordName = process.argv[3];
         try {
             await wordToHtmlByLibreOffice(wordName, './html-libre');
@@ -14,16 +15,25 @@ import {wordToHtmlByLibreOffice} from "../wordToHtmlConverter.js";
         }
     }
 
-    if (runtime === "libreMany"){
+    if (runtime === "libreMany") {
         try {
-            wordToHtmlByLibreOffice("1.doc", './html-libre');
-            wordToHtmlByLibreOffice("2.docx", './html-libre');
-            wordToHtmlByLibreOffice("3.docx", './html-libre');
+            wordToHtmlByLibreOffice("1.doc", './html-libre').then(() => {
+                    console.log('Conversion completed successfully')
+                }
+            )
+            wordToHtmlByLibreOffice("2.docx", './html-libre').then(() => {
+                    console.log('Conversion completed successfully')
+                }
+            )
+            wordToHtmlByLibreOffice("3.docx", './html-libre').then(() => {
+                    console.log('Conversion completed successfully')
+                }
+            )
         } catch (err) {
             console.error('Error at test.js:', err);
         }
     }
 
-    const endTime = new Date().getTime();
-    console.log(`Time spent: ${endTime - startTime} ms. Runtime: ${runtime}`);
 })();
+const endTime = new Date().getTime();
+console.log(`Time spent: ${endTime - startTime} ms. `);
